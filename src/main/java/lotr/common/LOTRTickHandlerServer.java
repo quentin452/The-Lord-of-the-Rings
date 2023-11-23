af774fd9f09ffcac1a9c1e46dc478384d7f89292
quentin452 <42176772+quentin452@users.noreply.github.com>
2023-11-23 06:04:12 +0100

    Update LOTRTickHandlerServer.java

--- src/main/java/lotr/common/LOTRTickHandlerServer.java
+++ src/main/java/lotr/common/LOTRTickHandlerServer.java
@@ -48,171 +48,175 @@ public class LOTRTickHandlerServer {
 		FMLCommonHandler.instance().bus().register(this);
 	}
 
-	@SubscribeEvent
-	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-		EntityPlayer player = event.player;
-		World world = player.worldObj;
-		if (world == null || world.isRemote) {
-			return;
-		}
-		if (player instanceof EntityPlayerMP) {
-			EntityPlayerMP entityplayer = (EntityPlayerMP) player;
+    @SubscribeEvent
+    public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+        int[] portalLocation;
+        EntityPlayer player = event.player;
+        World world = player.worldObj;
+        if (world == null || world.isRemote) {
+            return;
+        }
+        if (player instanceof EntityPlayerMP) {
+            EntityPlayerMP entityplayer = (EntityPlayerMP) player;
             if (event.phase == TickEvent.Phase.START && entityplayer.playerNetServerHandler != null && !(entityplayer.playerNetServerHandler instanceof LOTRNetHandlerPlayServer && LOTRConfig.enableAttackCooldown)) {
-				entityplayer.playerNetServerHandler = new LOTRNetHandlerPlayServer(MinecraftServer.getServer(), entityplayer.playerNetServerHandler.netManager, entityplayer);
-			}
-			if (event.phase == TickEvent.Phase.END) {
-				List items;
-				EntityItem item;
-				ItemStack heldItem;
-				LOTRLevelData.getData(entityplayer).onUpdate(entityplayer, (WorldServer) world);
-				NetHandlerPlayServer netHandler = entityplayer.playerNetServerHandler;
-				if (netHandler instanceof LOTRNetHandlerPlayServer) {
-					((LOTRNetHandlerPlayServer) netHandler).update();
-				}
-				heldItem = entityplayer.getHeldItem();
-				if (heldItem != null && (heldItem.getItem() instanceof ItemWritableBook || heldItem.getItem() instanceof ItemEditableBook)) {
-					entityplayer.func_143004_u();
-				}
-				if (entityplayer.dimension == 0 && LOTRLevelData.madePortal == 0) {
-					items = world.getEntitiesWithinAABB(EntityItem.class, entityplayer.boundingBox.expand(16.0, 16.0, 16.0));
-					for (Object obj : items) {
-						item = (EntityItem) obj;
-						if (LOTRLevelData.madePortal != 0 || item.getEntityItem() == null || item.getEntityItem().getItem() != LOTRMod.goldRing || !item.isBurning()) {
-							continue;
-						}
-						LOTRLevelData.setMadePortal(1);
-						LOTRLevelData.markOverworldPortalLocation(MathHelper.floor_double(item.posX), MathHelper.floor_double(item.posY), MathHelper.floor_double(item.posZ));
-						item.setDead();
-						world.createExplosion(entityplayer, item.posX, item.posY + 3.0, item.posZ, 3.0f, true);
-						LOTREntityPortal portal = new LOTREntityPortal(world);
-						portal.setLocationAndAngles(item.posX, item.posY + 3.0, item.posZ, 0.0f, 0.0f);
-						world.spawnEntityInWorld(portal);
-					}
-				}
-				if (entityplayer.dimension == 0 || entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) {
-					items = world.getEntitiesWithinAABB(EntityItem.class, entityplayer.boundingBox.expand(16.0, 16.0, 16.0));
-					for (Object obj : items) {
-						boolean foundPortalLocation;
-						int k1;
-						int[] portalLocation;
-						int i1;
-						item = (EntityItem) obj;
-						if (item.getEntityItem() == null) {
-							continue;
-						}
-						int i = MathHelper.floor_double(item.posX);
-						int j = MathHelper.floor_double(item.posY);
-						int k = MathHelper.floor_double(item.posZ);
-						ItemStack itemstack = item.getEntityItem();
-						if ((LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.LOTHLORIEN) >= 1.0f || LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.HIGH_ELF) >= 1.0f) && (itemstack.getItem() == Item.getItemFromBlock(LOTRMod.elanor) || itemstack.getItem() == Item.getItemFromBlock(LOTRMod.niphredil))) {
-							foundPortalLocation = false;
-							portalLocation = new int[3];
-							for (i1 = i - 2; !foundPortalLocation && i1 <= i + 2; ++i1) {
-								for (k1 = k - 2; !foundPortalLocation && k1 <= k + 2; ++k1) {
-									if (!((LOTRBlockPortal) LOTRMod.elvenPortal).isValidPortalLocation(world, i1, j, k1, false)) {
-										continue;
-									}
-									foundPortalLocation = true;
-									portalLocation = new int[]{i1, j, k1};
-								}
-							}
-							if (foundPortalLocation) {
-								item.setDead();
-								for (i1 = -1; i1 <= 1; ++i1) {
-									for (k1 = -1; k1 <= 1; ++k1) {
-										world.setBlock(portalLocation[0] + i1, portalLocation[1], portalLocation[2] + k1, LOTRMod.elvenPortal, 0, 2);
-									}
-								}
-							}
-						}
-						if (LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.MORDOR) < 1.0f && LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.ANGMAR) < 1.0f && LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.DOL_GULDUR) < 1.0f || !LOTRMod.isOreNameEqual(itemstack, "bone")) {
-							continue;
-						}
-						foundPortalLocation = false;
-						portalLocation = new int[3];
-						for (i1 = i - 2; !foundPortalLocation && i1 <= i + 2; ++i1) {
-							for (k1 = k - 2; !foundPortalLocation && k1 <= k + 2; ++k1) {
-								if (!((LOTRBlockPortal) LOTRMod.morgulPortal).isValidPortalLocation(world, i1, j, k1, false)) {
-									continue;
-								}
-								foundPortalLocation = true;
-								portalLocation = new int[]{i1, j, k1};
-							}
-						}
-						if (!foundPortalLocation) {
-							continue;
-						}
-						item.setDead();
-						for (i1 = -1; i1 <= 1; ++i1) {
-							for (k1 = -1; k1 <= 1; ++k1) {
-								world.setBlock(portalLocation[0] + i1, portalLocation[1], portalLocation[2] + k1, LOTRMod.morgulPortal, 0, 2);
-							}
-						}
-					}
-				}
-				if ((entityplayer.dimension == 0 || entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) && playersInPortals.containsKey(entityplayer)) {
-					int i;
-					List portals = world.getEntitiesWithinAABB(LOTREntityPortal.class, entityplayer.boundingBox.expand(8.0, 8.0, 8.0));
-					boolean inPortal = false;
-					for (i = 0; i < portals.size(); ++i) {
-						LOTREntityPortal portal = (LOTREntityPortal) portals.get(i);
-						if (!portal.boundingBox.intersectsWith(entityplayer.boundingBox)) {
-							continue;
-						}
-						inPortal = true;
-						break;
-					}
-					if (inPortal) {
-						i = (Integer) playersInPortals.get(entityplayer);
-						i++;
-						playersInPortals.put(entityplayer, i);
-						if (i >= 100) {
-							int dimension = 0;
-							if (entityplayer.dimension == 0) {
-								dimension = LOTRDimension.MIDDLE_EARTH.dimensionID;
-							} else if (entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) {
-								dimension = 0;
-							}
-							MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension(entityplayer, dimension, new LOTRTeleporter(DimensionManager.getWorld(dimension), true));
-							playersInPortals.remove(entityplayer);
-						}
-					} else {
-						playersInPortals.remove(entityplayer);
-					}
-				}
-				updatePlayerInPortal(entityplayer, playersInElvenPortals, (LOTRBlockPortal) LOTRMod.elvenPortal);
-				updatePlayerInPortal(entityplayer, playersInMorgulPortals, (LOTRBlockPortal) LOTRMod.morgulPortal);
-				if (entityplayer.dimension == LOTRDimension.UTUMNO.dimensionID) {
-					int i = MathHelper.floor_double(entityplayer.posX);
-					int j = MathHelper.floor_double(entityplayer.boundingBox.minY);
-					int k = MathHelper.floor_double(entityplayer.posZ);
-					int range = 32;
-					for (int l = 0; l < 60; ++l) {
-						int i1 = i + world.rand.nextInt(range) - world.rand.nextInt(range);
-						int j1 = j + world.rand.nextInt(range) - world.rand.nextInt(range);
-						int k1 = k + world.rand.nextInt(range) - world.rand.nextInt(range);
-						if (LOTRUtumnoLevel.forY(j1) == LOTRUtumnoLevel.ICE) {
-							Block block = world.getBlock(i1, j1, k1);
-							int meta = world.getBlockMetadata(i1, j1, k1);
-							if (block.getMaterial() == Material.water && meta == 0) {
-								world.setBlock(i1, j1, k1, Blocks.ice, 0, 3);
-							}
-						}
-						if (LOTRUtumnoLevel.forY(j1) != LOTRUtumnoLevel.FIRE) {
-							continue;
-						}
-						Block block = world.getBlock(i1, j1, k1);
-						int meta = world.getBlockMetadata(i1, j1, k1);
-						if (block.getMaterial() != Material.water || meta != 0) {
-							continue;
-						}
-						world.setBlock(i1, j1, k1, Blocks.air, 0, 3);
-						LOTRWorldProviderUtumno.doEvaporateFX(world, i1, j1, k1);
-					}
-				}
-			}
-		}
-	}
+                entityplayer.playerNetServerHandler = new LOTRNetHandlerPlayServer(MinecraftServer.getServer(), entityplayer.playerNetServerHandler.netManager, entityplayer);
+            }
+            if (event.phase == TickEvent.Phase.END) {
+                List items;
+                EntityItem item;
+                ItemStack heldItem;
+                LOTRLevelData.getData(entityplayer).onUpdate(entityplayer, (WorldServer) world);
+                NetHandlerPlayServer netHandler = entityplayer.playerNetServerHandler;
+                if (netHandler instanceof LOTRNetHandlerPlayServer) {
+                    ((LOTRNetHandlerPlayServer) netHandler).update();
+                }
+                heldItem = entityplayer.getHeldItem();
+                if (heldItem != null && (heldItem.getItem() instanceof ItemWritableBook || heldItem.getItem() instanceof ItemEditableBook)) {
+                    entityplayer.func_143004_u();
+                }
+                if (entityplayer.dimension == 0 && LOTRLevelData.madePortal == 0) {
+                    items = world.getEntitiesWithinAABB(EntityItem.class, entityplayer.boundingBox.expand(16.0, 16.0, 16.0));
+                    for (Object obj : items) {
+                        item = (EntityItem) obj;
+                        if (LOTRLevelData.madePortal != 0 || item.getEntityItem() == null || item.getEntityItem().getItem() != LOTRMod.goldRing || !item.isBurning()) {
+                            continue;
+                        }
+                        LOTRLevelData.setMadePortal(1);
+                        LOTRLevelData.markOverworldPortalLocation(MathHelper.floor_double(item.posX), MathHelper.floor_double(item.posY), MathHelper.floor_double(item.posZ));
+                        item.setDead();
+                        world.createExplosion(entityplayer, item.posX, item.posY + 3.0, item.posZ, 3.0f, true);
+                        LOTREntityPortal portal = new LOTREntityPortal(world);
+                        portal.setLocationAndAngles(item.posX, item.posY + 3.0, item.posZ, 0.0f, 0.0f);
+                        world.spawnEntityInWorld(portal);
+                    }
+                }
+                if (entityplayer.dimension == 0 || entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) {
+                    items = world.getEntitiesWithinAABB(EntityItem.class, entityplayer.boundingBox.expand(16.0, 16.0, 16.0));
+                    for (Object obj : items) {
+                        boolean foundPortalLocation;
+                        int k1;
+                        int i1;
+                        item = (EntityItem) obj;
+                        if (item.getEntityItem() == null) {
+                            continue;
+                        }
+                        int i = MathHelper.floor_double(item.posX);
+                        int j = MathHelper.floor_double(item.posY);
+                        int k = MathHelper.floor_double(item.posZ);
+                        ItemStack itemstack = item.getEntityItem();
+                        if ((LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.LOTHLORIEN) >= 1.0f || LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.HIGH_ELF) >= 1.0f) && (itemstack.getItem() == Item.getItemFromBlock(LOTRMod.elanor) || itemstack.getItem() == Item.getItemFromBlock(LOTRMod.niphredil))) {
+                            foundPortalLocation = false;
+                            portalLocation = new int[3];
+                            for (i1 = i - 2; !foundPortalLocation && i1 <= i + 2; ++i1) {
+                                for (k1 = k - 2; !foundPortalLocation && k1 <= k + 2; ++k1) {
+                                    if (!((LOTRBlockPortal) LOTRMod.elvenPortal).isValidPortalLocation(world, i1, j, k1, false)) {
+                                        continue;
+                                    }
+                                    foundPortalLocation = true;
+                                    portalLocation[0] = i1;
+                                    portalLocation[1] = j;
+                                    portalLocation[2] = k1;
+                                }
+                            }
+                            if (foundPortalLocation) {
+                                item.setDead();
+                                for (i1 = -1; i1 <= 1; ++i1) {
+                                    for (k1 = -1; k1 <= 1; ++k1) {
+                                        world.setBlock(portalLocation[0] + i1, portalLocation[1], portalLocation[2] + k1, LOTRMod.elvenPortal, 0, 2);
+                                    }
+                                }
+                            }
+                        }
+                        if (LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.MORDOR) < 1.0f && LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.ANGMAR) < 1.0f && LOTRLevelData.getData(entityplayer).getAlignment(LOTRFaction.DOL_GULDUR) < 1.0f || !LOTRMod.isOreNameEqual(itemstack, "bone")) {
+                            continue;
+                        }
+                        foundPortalLocation = false;
+                        portalLocation = new int[3];
+                        for (i1 = i - 2; !foundPortalLocation && i1 <= i + 2; ++i1) {
+                            for (k1 = k - 2; !foundPortalLocation && k1 <= k + 2; ++k1) {
+                                if (!((LOTRBlockPortal) LOTRMod.morgulPortal).isValidPortalLocation(world, i1, j, k1, false)) {
+                                    continue;
+                                }
+                                foundPortalLocation = true;
+                                portalLocation[0] = i1;
+                                portalLocation[1] = j;
+                                portalLocation[2] = k1;
+                            }
+                        }
+                        if (!foundPortalLocation) {
+                            continue;
+                        }
+                        item.setDead();
+                        for (i1 = -1; i1 <= 1; ++i1) {
+                            for (k1 = -1; k1 <= 1; ++k1) {
+                                world.setBlock(portalLocation[0] + i1, portalLocation[1], portalLocation[2] + k1, LOTRMod.morgulPortal, 0, 2);
+                            }
+                        }
+                    }
+                }
+                if ((entityplayer.dimension == 0 || entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) && playersInPortals.containsKey(entityplayer)) {
+                    int i;
+                    List portals = world.getEntitiesWithinAABB(LOTREntityPortal.class, entityplayer.boundingBox.expand(8.0, 8.0, 8.0));
+                    boolean inPortal = false;
+                    for (i = 0; i < portals.size(); ++i) {
+                        LOTREntityPortal portal = (LOTREntityPortal) portals.get(i);
+                        if (!portal.boundingBox.intersectsWith(entityplayer.boundingBox)) {
+                            continue;
+                        }
+                        inPortal = true;
+                        break;
+                    }
+                    if (inPortal) {
+                        i = (Integer) playersInPortals.get(entityplayer);
+                        i++;
+                        playersInPortals.put(entityplayer, i);
+                        if (i >= 100) {
+                            int dimension = 0;
+                            if (entityplayer.dimension == 0) {
+                                dimension = LOTRDimension.MIDDLE_EARTH.dimensionID;
+                            } else if (entityplayer.dimension == LOTRDimension.MIDDLE_EARTH.dimensionID) {
+                                dimension = 0;
+                            }
+                            MinecraftServer.getServer().getConfigurationManager().transferPlayerToDimension(entityplayer, dimension, new LOTRTeleporter(DimensionManager.getWorld(dimension), true));
+                            playersInPortals.remove(entityplayer);
+                        }
+                    } else {
+                        playersInPortals.remove(entityplayer);
+                    }
+                }
+                updatePlayerInPortal(entityplayer, playersInElvenPortals, (LOTRBlockPortal) LOTRMod.elvenPortal);
+                updatePlayerInPortal(entityplayer, playersInMorgulPortals, (LOTRBlockPortal) LOTRMod.morgulPortal);
+                if (entityplayer.dimension == LOTRDimension.UTUMNO.dimensionID) {
+                    int i = MathHelper.floor_double(entityplayer.posX);
+                    int j = MathHelper.floor_double(entityplayer.boundingBox.minY);
+                    int k = MathHelper.floor_double(entityplayer.posZ);
+                    int range = 32;
+                    for (int l = 0; l < 60; ++l) {
+                        int i1 = i + world.rand.nextInt(range) - world.rand.nextInt(range);
+                        int j1 = j + world.rand.nextInt(range) - world.rand.nextInt(range);
+                        int k1 = k + world.rand.nextInt(range) - world.rand.nextInt(range);
+                        if (LOTRUtumnoLevel.forY(j1) == LOTRUtumnoLevel.ICE) {
+                            Block block = world.getBlock(i1, j1, k1);
+                            int meta = world.getBlockMetadata(i1, j1, k1);
+                            if (block.getMaterial() == Material.water && meta == 0) {
+                                world.setBlock(i1, j1, k1, Blocks.ice, 0, 3);
+                            }
+                        }
+                        if (LOTRUtumnoLevel.forY(j1) != LOTRUtumnoLevel.FIRE) {
+                            continue;
+                        }
+                        Block block = world.getBlock(i1, j1, k1);
+                        int meta = world.getBlockMetadata(i1, j1, k1);
+                        if (block.getMaterial() != Material.water || meta != 0) {
+                            continue;
+                        }
+                        world.setBlock(i1, j1, k1, Blocks.air, 0, 3);
+                        LOTRWorldProviderUtumno.doEvaporateFX(world, i1, j1, k1);
+                    }
+                }
+            }
+        }
+    }
 
 	@SubscribeEvent
 	public void onWorldTick(TickEvent.WorldTickEvent event) {
