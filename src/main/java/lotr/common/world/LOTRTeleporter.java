1accbc6651f6a3f0fa8aa27a2ffc1aeb23dd3d69
QUENTIN453 <quentinlamauve@gmail.com>
2023-11-26 00:49:40 +0100

    Add null checks in LOTRTeleporter

--- src/main/java/lotr/common/world/LOTRTeleporter.java
+++ src/main/java/lotr/common/world/LOTRTeleporter.java
@@ -9,40 +9,45 @@ import net.minecraft.world.Teleporter;
 import net.minecraft.world.WorldServer;
 
 public class LOTRTeleporter extends Teleporter {
-	public WorldServer world;
-	public boolean makeRingPortal;
+    public WorldServer world;
+    public boolean makeRingPortal;
 
-	public LOTRTeleporter(WorldServer worldserver, boolean flag) {
-		super(worldserver);
-		world = worldserver;
-		makeRingPortal = flag;
-	}
+    public LOTRTeleporter(WorldServer worldserver, boolean flag) {
+        super(worldserver);
+        world = worldserver;
+        makeRingPortal = flag;
+    }
 
-	@Override
-	public void placeInPortal(Entity entity, double d, double d1, double d2, float f) {
-		int j;
-		int k;
-		int i;
-		if (world.provider.dimensionId == LOTRDimension.MIDDLE_EARTH.dimensionID) {
-			i = 0;
-			k = 0;
-			j = LOTRMod.getTrueTopBlock(world, i, k);
-		} else {
-			i = LOTRLevelData.overworldPortalX;
-			k = LOTRLevelData.overworldPortalZ;
-			j = LOTRLevelData.overworldPortalY;
-		}
-		entity.setLocationAndAngles(i + 0.5, j + 1.0, k + 0.5, entity.rotationYaw, 0.0f);
-		if (world.provider.dimensionId == LOTRDimension.MIDDLE_EARTH.dimensionID && LOTRLevelData.madeMiddleEarthPortal == 0) {
-			LOTRLevelData.setMadeMiddleEarthPortal(1);
-			if (makeRingPortal) {
-				if (world.provider instanceof LOTRWorldProviderMiddleEarth) {
-					((LOTRWorldProviderMiddleEarth) world.provider).setRingPortalLocation(i, j, k);
-				}
-				LOTREntityPortal portal = new LOTREntityPortal(world);
-				portal.setLocationAndAngles(i + 0.5, j + 3.5, k + 0.5, 0.0f, 0.0f);
-				world.spawnEntityInWorld(portal);
-			}
-		}
-	}
+    @Override
+    public void placeInPortal(Entity entity, double d, double d1, double d2, float f) {
+        if (world == null || world.provider == null) {
+            return;
+        }
+
+        int i, j, k;
+        if (world.provider.dimensionId == LOTRDimension.MIDDLE_EARTH.dimensionID) {
+            i = 0;
+            k = 0;
+            j = LOTRMod.getTrueTopBlock(world, i, k);
+        } else {
+            i = LOTRLevelData.overworldPortalX;
+            k = LOTRLevelData.overworldPortalZ;
+            j = LOTRLevelData.overworldPortalY;
+        }
+
+        if (entity == null) {return;
+        }
+
+        entity.setLocationAndAngles(i + 0.5, j + 1.0, k + 0.5, entity.rotationYaw, 0.0f);
+
+        if (world.provider.dimensionId == LOTRDimension.MIDDLE_EARTH.dimensionID && LOTRLevelData.madeMiddleEarthPortal == 0) {
+            LOTRLevelData.setMadeMiddleEarthPortal(1);
+            if (makeRingPortal && world.provider instanceof LOTRWorldProviderMiddleEarth) {
+                ((LOTRWorldProviderMiddleEarth) world.provider).setRingPortalLocation(i, j, k);
+                LOTREntityPortal portal = new LOTREntityPortal(world);
+                portal.setLocationAndAngles(i + 0.5, j + 3.5, k + 0.5, 0.0f, 0.0f);
+                world.spawnEntityInWorld(portal);
+            }
+        }
+    }
 }
